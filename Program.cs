100c34ab0a359fa21bd70024baa6ec30eb5a9c00
Roshani-Poojari <135858937+Roshani-Poojari@users.noreply.github.com>
2024-07-11 21:44:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParseDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            if (int.TryParse(Console.ReadLine(), out int myNumber))
+            {
+                int squareNumber = myNumber * myNumber;
+                Console.WriteLine("Square of "+myNumber+" is: " + squareNumber);
+            }
+            else
+                Console.WriteLine("Invalid input! Enter a number!!!");
+        }
+    }
+}
